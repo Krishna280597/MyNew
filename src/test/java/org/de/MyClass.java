9c22217a7cfc848d344afd454fdf56534b431ea2
Krishna280597 <ckrishnakrishnapriya@gmail.com>
2021-06-20 11:27:42 +0530

    krish updated code

--- src/test/java/org/de/MyClass.java
+++ src/test/java/org/de/MyClass.java
@@ -3,6 +3,6 @@ package org.de;
 public class MyClass {
 public static void main(String[] args) {
 	System.out.println("Welcome to GitHub");
-	System.out.println("Hii i created a branch priya");
+	System.out.println("i am krishna");
 }
 }
